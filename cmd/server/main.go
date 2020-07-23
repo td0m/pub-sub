75ce2ada51514e8f1fa7f1afc60251ae09c4b14b
Dominik Tarnowski <dom@tdom.dev>
2020-07-23 16:43:04 +0100

    feat: implement keepalive ping/pongs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+	// how often the server will ping clients for keepalive pongs
+	pingPeriod = time.Second * 10
+	// how long to wait for pongs
+	// must be larger than pingPeriod
+	pongTimeout = pingPeriod + time.Second*2
 )
 
 var upgrader = websocket.Upgrader{
@@ -18,22 +30,46 @@ type Message struct {
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
+	eventsParam := r.URL.Query().Get("events")
+	events := strings.Split(strings.ToLower(eventsParam), ",")
 	if err != nil {
-		log.Print("upgrade:", err)
+		log.Println("upgrade:", err)
 		return
 	}
+	log.Println(events)
+	go reader(c)
+	go writer(c)
+}
+
+func reader(c *websocket.Conn) {
 	defer c.Close()
+	c.SetReadDeadline(time.Now().Add(pongTimeout))
+	c.SetPongHandler(func(string) error {
+		c.SetReadDeadline(time.Now().Add(pongTimeout))
+		return nil
+	})
+
 	for {
 		var msg Message
 		err := c.ReadJSON(&msg)
 		if err != nil {
-			log.Print("read:", err)
-			return
+			log.Println("read: ", err)
 		}
-		err = c.WriteJSON(msg)
-		if err != nil {
-			log.Print("write:", err)
-			return
+		log.Println(msg)
+		// TODO: emit msg
+	}
+}
+
+func writer(c *websocket.Conn) {
+	defer c.Close()
+	ticker := time.NewTicker(pingPeriod)
+	for {
+		select {
+		case <-ticker.C:
+			c.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
 		}
 	}
 }
