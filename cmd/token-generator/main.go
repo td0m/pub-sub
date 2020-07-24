08162f44473219638b69b06c62bf19f32f22a979
Dominik Tarnowski <dom@tdom.dev>
2020-07-24 15:04:44 +0100

    feat: add token generator cmd

---
+++ cmd/token-generator/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/td0m/pub-sub/pkg/auth"
+)
+
+func main() {
+	user := readLine("user")
+	fmt.Println("Please enter permissions separated by a comma")
+	read, write := readLine("read permissions"), readLine("write permissions")
+	token, err := auth.CreateToken(user, arr(read), arr(write))
+	if err != nil {
+		fmt.Println("error: ", err)
+		return
+	}
+	fmt.Println("token: " + token)
+}
+
+func arr(s string) []string {
+	return strings.Split(s, ",")
+}
+
+func readLine(field string) string {
+	r := bufio.NewReader(os.Stdin)
+	fmt.Printf("%s: ", field)
+	text, _ := r.ReadString('\n')
+	return text[:len(text)-1]
+}
